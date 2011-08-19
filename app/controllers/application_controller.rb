1c1c97f670ef1454d59f0ed6600b0647b7cc5f91
Ivan Primorac <email>
2011-08-19 21:16:21 +0200

    !

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  def ivan
+    if 2 > 3
+
+    end
+  end
+end
---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,11 @@
+module PostsHelper
+
+
+  def func1
+    x = 5
+
+    list = ['dadada', '434343423']
+
+    num = list.forty_two
+  end
+end
